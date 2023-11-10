2ce4c2ce18850b918ccfab7dbd2a8263b0c34f1a
KazuyaAizawa2023 <136213764+KazuyaAizawa2023@users.noreply.github.com>
2023-11-10 17:17:35 +0900

    岩の生成の位置を画面上半分に変更

--- Assets/Scripts/RockMovement.cs
+++ Assets/Scripts/RockMovement.cs
@@ -1,23 +1,25 @@
-// 岩が垂直に落下する動きをつけるスクリプト
 using UnityEngine;
 
 public class RockMovement : MonoBehaviour
 {
-    public float fallSpeed = 5.0f; // 岩の落下速度
+    public float fallSpeed = 5.0f;        // 岩の落下速度
+    public float initialDelay = 2.0f;     // 初期遅延時間
 
     private Rigidbody rb;
 
     private void Start()
     {
-        // ゲームが始まった瞬間に実行される部分。
-        // 岩に物理法則（重力）を適用して、岩が自動的に下に落ちるようにする。
-
         rb = GetComponent<Rigidbody>();
 
-        // 岩に重力を適用して垂直に落下させる
-        rb.useGravity = true;
+        // 岩に重力を適用して、一定時間後に垂直に落下させる
+        rb.useGravity = false;
+        Invoke("StartFalling", initialDelay);
+    }
 
-        // 岩の初速度を設定し、下に向かって垂直に落下するようにする。
+    private void StartFalling()
+    {
+        // 初期遅延後に岩の初速度を設定し、下に向かって垂直に落下するようにする
+        rb.useGravity = true;
         rb.velocity = new Vector3(0, -fallSpeed, 0);
     }
 }
--- Assets/Scripts/RockSpawner.cs
+++ Assets/Scripts/RockSpawner.cs
@@ -4,6 +4,7 @@ public class RockSpawner : MonoBehaviour
 {
     public GameObject rockPrefab;    // 岩のプレハブ
     public float spawnInterval = 3.0f; // 岩の生成間隔（秒）
+    public float initialDelay = 2.0f;  // 初期遅延時間
 
     private Camera mainCamera;
     private float timer = 0.0f;
@@ -11,6 +12,9 @@ public class RockSpawner : MonoBehaviour
     private void Start()
     {
         mainCamera = Camera.main; // カメラを取得
+
+        // 最初の岩を生成
+        SpawnRockDelayed(initialDelay);
     }
 
     private void Update()
@@ -20,19 +24,29 @@ public class RockSpawner : MonoBehaviour
         // 指定の間隔ごとに岩を生成
         if (timer >= spawnInterval)
         {
-            SpawnRock(); // 岩を生成するメソッドを呼び出す
+            SpawnRockDelayed(0.0f); // 岩を生成するメソッドを呼び出す（初期遅延なし）
             timer = 0.0f; // タイマーをリセット
         }
+    }
+
+    void SpawnRockDelayed(float delay)
+    {
+        // 指定の遅延時間後に岩を生成
+        Invoke("SpawnRock", delay);
     }
 
     void SpawnRock()
     {
         // ランダムな位置を生成（画面内のランダムな位置を取得）
-        Vector3 randomViewportPoint = new Vector3(Random.value, Random.value, 0);
+        Vector3 randomViewportPoint = new Vector3(Random.value, Random.Range(0.5f, 1.0f), 0); // Y座標を1.0fに設定して画面の上部に生成
         Vector3 randomWorldPoint = mainCamera.ViewportToWorldPoint(randomViewportPoint);
 
         // 岩を生成し、手前に配置
         GameObject rock = Instantiate(rockPrefab, randomWorldPoint, Quaternion.identity);
         rock.transform.position = new Vector3(rock.transform.position.x, rock.transform.position.y, 0); // Z座標を手前に配置
+
+        // 生成された岩に RockMovement スクリプトをアタッチ
+        rock.AddComponent<RockMovement>();
     }
 }
+
