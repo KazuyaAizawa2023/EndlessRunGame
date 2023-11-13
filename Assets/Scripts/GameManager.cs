9b76a4eb007997e1254b47a9d823186bef3d8e95
KazuyaAizawa2023 <136213764+KazuyaAizawa2023@users.noreply.github.com>
2023-11-13 19:33:19 +0900

    レベルアップした際の変更点

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,31 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Text scoreText;             // スコアを表示するテキスト
-    private float score = 0.0f;         // 現在のスコア
+    public Text scoreText;                     // スコアを表示するテキスト
+    private float score = 0.0f;                // 現在のスコア
     public float scoreIncreaseAmount = 1.0f;   // スコアが増える量
-    private int currentLevel = 1;       // 現在のレベル
+    private int currentLevel = 1;               // 現在のレベル
+
+    private PlayerMovement playerMovement;
+    private RockSpawner rockSpawner;
 
     void Start()
     {
-        UpdateScoreText();  // ゲーム開始時にスコアを表示
-        StartCoroutine(IncreaseScore());   // スコアを増やすコルーチンを開始
+        // PlayerMovement と RockSpawner を探して参照を取得
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        rockSpawner = FindObjectOfType<RockSpawner>();
+
+        UpdateScoreText();
+        StartCoroutine(IncreaseScore());
     }
 
     void UpdateScoreText()
     {
         // スコアとレベルをテキストに表示
-        scoreText.text = "Score: " + Mathf.FloorToInt(score).ToString() + " | Level: " + currentLevel.ToString();
+        if (scoreText != null)  // null チェックを追加
+        {
+            scoreText.text = "Score: " + Mathf.FloorToInt(score).ToString() + " | Level: " + currentLevel.ToString();
+        }
     }
 
     IEnumerator IncreaseScore()
@@ -28,7 +38,31 @@ public class GameManager : MonoBehaviour
             // 一定の間隔ごとにスコアが増える
             score += scoreIncreaseAmount;
             UpdateScoreText();   // スコア表示を更新
+
+            // スコアが50の倍数のときにレベルアップ
+            if (Mathf.FloorToInt(score) % 50 == 0)
+            {
+                LevelUp();
+            }
+
             yield return new WaitForSeconds(1.0f); // 1秒ごとにスコアが増える例
         }
     }
+
+    void LevelUp()
+    //これにより、LevelUp メソッドが呼ばれると、プレイヤーの速度が10%増加し、
+    //障害物の生成間隔が10%減少します。
+    {
+        // レベルアップ時の処理
+        currentLevel++;
+
+        // プレイヤーの速度を15%増加
+        playerMovement.moveSpeed *= 1.15f;
+
+        // 障害物の生成間隔を30%減少
+        rockSpawner.spawnInterval *= 0.7f;
+
+        // ここで必要なレベルアップ時の処理を追加
+    }
+    
 }
