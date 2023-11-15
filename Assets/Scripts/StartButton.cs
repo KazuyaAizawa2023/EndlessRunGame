d6887dab97ab356e25a2abfe7a42edb610fc379d
KazuyaAizawa2023 <136213764+KazuyaAizawa2023@users.noreply.github.com>
2023-11-15 17:55:10 +0900

    タイトル画面の作成、スタートボタン作成、ゲームシーンに転移

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public void Start()
+    {
+        var button = GetComponent<Button>();
+
+        //ボタンを押したときのリスナー設定
+        button.onClick.AddListener(() =>
+        {
+            //シーン転移の際にはSceneManagerを使用する
+            SceneManager.LoadScene("GameScene");
+        });
+    }
+
+}
