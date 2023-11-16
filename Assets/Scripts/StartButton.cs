d709f9cb18afbf66ed148184a2490ffbf422345b
KazuyaAizawa2023 <136213764+KazuyaAizawa2023@users.noreply.github.com>
2023-11-16 19:03:16 +0900

    説明シーンを入れたかったもののエラーが出るのでやめ

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    
     public void Start()
     {
         var button = GetComponent<Button>();
 
-        //ボタンを押したときのリスナー設定
+        // ボタンを押したときのリスナー設定
         button.onClick.AddListener(() =>
         {
-            //シーン転移の際にはSceneManagerを使用する
+            // 説明シーンをロード
             SceneManager.LoadScene("GameScene");
         });
-    }
 
-}
+    }
+}   
