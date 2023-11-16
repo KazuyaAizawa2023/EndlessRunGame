10436c3807f72c05a24fbc09ca2879b9e411405f
KazuyaAizawa2023 <136213764+KazuyaAizawa2023@users.noreply.github.com>
2023-11-16 16:13:31 +0900

    当たり判定の追加

--- Assets/Scripts/RockMovement.cs
+++ Assets/Scripts/RockMovement.cs
@@ -5,23 +5,35 @@ public class RockMovement : MonoBehaviour
     public float fallSpeed = 5.0f;        // 岩の落下速度
     public float initialDelay = 2.0f;     // 初期遅延時間
 
-    private Rigidbody rb;
+    private Rigidbody2D rb2D;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb2D = GetComponent<Rigidbody2D>();
 
         // 岩に重力を適用して、一定時間後に垂直に落下させる
-        rb.useGravity = false;
-        Invoke("StartFalling", initialDelay);
+        rb2D.gravityScale = 1.0f; // 通常の重力を適用
+        rb2D.velocity = new Vector2(0, -fallSpeed); // 2D向けに修正
     }
 
-    private void StartFalling()
+
+
+    // 衝突した瞬間に呼び出されるメソッド
+    private void OnCollisionEnter(Collision collision)
     {
-        // 初期遅延後に岩の初速度を設定し、下に向かって垂直に落下するようにする
-        rb.useGravity = true;
-        rb.velocity = new Vector3(0, -fallSpeed, 0);
+        // 衝突した相手がプレイヤーだった場合
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // 速度をゼロにする
+            rb2D.velocity = Vector3.zero;
+        }
     }
+    //private void StartFalling()
+    //{
+    //    // 初期遅延後に岩の初速度を設定し、下に向かって垂直に落下するようにする
+    //    rb.useGravity = true;
+    //    rb.velocity = new Vector3(0, -fallSpeed, 0);
+    //}
 
     //private void Update()
     //{
