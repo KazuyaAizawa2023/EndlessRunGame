a1e0bb96933ea24d68b1a3e89f957dc5cce9cc55
KazuyaAizawa2023 <136213764+KazuyaAizawa2023@users.noreply.github.com>
2023-11-16 19:04:17 +0900

    説明シーンのスクリプトを残しておく

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -17,4 +17,35 @@ public class StartButton : MonoBehaviour
         });
 
     }
-}   
+}
+
+
+
+
+//public class StartButton : MonoBehaviour
+//{
+//    public float explanationDuration = 2.0f; // 説明の表示時間（秒）
+
+//    public void Start()
+//    {
+//        var button = GetComponent<Button>();
+
+//        // ボタンを押したときのリスナー設定
+//        button.onClick.AddListener(() =>
+//        {
+//            // 説明シーンをロード
+//            SceneManager.LoadScene("ExplanationScene");
+//        });
+
+//        // 一定時間後にゲームスタート
+//        Invoke("StartGame", explanationDuration);
+//    }
+
+//    // ゲームスタート時の処理
+//    private void StartGame()
+//    {
+//        Debug.Log("StartGame method called");
+//        // ゲームシーンをロード
+//        SceneManager.LoadScene("GameScene");
+//    }
+//}
